8cf7d3448bcb47be39fd95488e95b2b54eb7eadc
Luis Chico <luischico@gmail.com>
2017-11-01 21:31:56 -0300

    Update some correccions and chapter1

--- compilar.sh
+++ compilar.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
+rm *log *lot *lof *out *toc *aux
+rm Chapters/*aux
+
+
 pdflatex memoria.tex
 bibtex memoria
 pdflatex memoria.tex
 pdflatex memoria.tex
 
-rm *log *lot *lof *out *toc
 
 
